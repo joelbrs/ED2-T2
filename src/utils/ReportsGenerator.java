dee722a5aa82a3d018878788aecae0c3b5dd70c8
Joel Filho <joelfilho02812@gmail.com>
2023-11-04 09:23:40 -0300

    feat: add ReportsGenerators documentation

--- src/utils/ReportsGenerator.java
+++ src/utils/ReportsGenerator.java
@@ -5,11 +5,22 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Classe utilitária que gera relatórios com os resultados de cada teste de nossas implementações
+ *
+ * */
 public class ReportsGenerator {
 
+    /**
+     * Constantes referenciando o pacote de pacotes de cada problema resolvido
+     * */
     public static final String PROBLEM_1_REPORTS_PATH = "src/reports/problem_1/";
     public static final String PROBLEM_2_REPORTS_PATH = "src/reports/problem_2/";
 
+
+    /**
+     * Método que gera os relatórios em .txt de acordo com os resultados obtidos que são passados como String
+     * */
     public static void generate(String text, String name, String path) throws IOException {
         File file = new File(path + name + ".txt");
 
