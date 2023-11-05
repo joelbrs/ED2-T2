8786f6f2a7e41775dce0f555da81610298b58acb
Joel Filho <joelfilho02812@gmail.com>
2023-11-05 13:54:50 -0300

    feat: add Problem_1 & Problem_2 calls at Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import problems.Problem_1;
+import problems.Problem_2;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Problem_1.main(args);
+        Problem_2.main(args);
     }
 }
